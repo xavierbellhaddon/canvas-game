0649dbb8d413f8865b22f12788911e7e2696ce67
Austin Bell <snarkeet@gmail.com>
2020-11-14 21:15:36 -0500

    shrink enemies on hit

--- index.js
+++ index.js
@@ -136,10 +136,19 @@ enemies.forEach((enemy, index) => {
     projectiles.forEach((projectile, projectileIndex) => {
         const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y);
         if (dist - enemy.radius - projectile.radius < 1) {
-            setTimeout(() => {
-                enemies.splice(index, 1);
-                projectiles.splice(projectileIndex, 1);
-            }, 0);
+            if (enemy.radius - 10 > 5) {
+                gsap.to(enemy, {
+                    radius: enemy.radius - 10
+                })
+                setTimeout(() => {
+                    projectiles.splice(projectileIndex, 1);
+                }, 0);
+            } else {
+                setTimeout(() => {
+                    enemies.splice(index, 1);
+                    projectiles.splice(projectileIndex, 1);
+                }, 0);
+            }
         }
     });
 });
